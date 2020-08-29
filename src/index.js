580e1cbca027ee3389fe1311ee60d0e41392108f
josimar-jr <shikamarujunior@gmail.com>
2020-08-29 13:31:48 -0300

    establishes ci with travis - attempt 1

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ const port = Number(process.env.PORT) || 3000
 app.listen(port, () => {
   const serverStartDatetime = new Date()
   console.info(`application running - ${serverStartDatetime.toString()}`)
+  console.info(`node version - ${process.version}`)
 })
